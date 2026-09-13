b39ecdcad59e89d3b830557f1f293f8bda4c9e69
mateushahndev <mateushahn333@gmail.com>
2026-09-13 12:27:22 -0300

    página 404

---
+++ views/errors/error.php
@@ -0,0 +1,5 @@
+<div style="text-align:center;padding:3rem 1rem;">
+    <h1 class="auth-title" style="font-size:4rem;margin-bottom:.5rem">404</h1>
+    <p class="auth-subtitle">Página não encontrada</p>
+    <a href="/" class="btn btn-primary">Voltar ao início</a>
+</div>

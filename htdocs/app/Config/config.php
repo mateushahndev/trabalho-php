3e70ae04d605cf97ba1c70f2030035bc8e734dc7
Geraldo Nascimento <geraldogabriel@gmail.com>
2026-09-19 16:18:41 -0300

    Adicionar arquivo básico de configuração
    Retorna um array associativo com detalhes de configuração.

---
+++ htdocs/app/Config/config.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'app' => [
+        'name' => 'Opções B3',
+        'debug' => true,
+    ],
+
+    'database' => [
+        'host'     => 'localhost',
+        'port'     => 5432,
+        'dbname'   => 'opcoes_b3',
+        'user'     => 'opcoes_user',
+        'password' => 'quie/b7Aevi0UoXioKoo',
+    ],
+
+    'session' => [
+        'name' => 'opcoes_b3_session',
+    ],
+];
